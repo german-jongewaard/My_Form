26c3078e401cdd3d4f8023e87cabc8b0ac6794da
german <germanjongewaard@gmail.com>
2017-11-07 02:58:58 +0100

    first commit.

--- app/src/main/java/dev/com/jongewaard/my_form/ThirdActivity.java
+++ app/src/main/java/dev/com/jongewaard/my_form/ThirdActivity.java
@@ -1,11 +1,14 @@
 package dev.com.jongewaard.my_form;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by german on 6-11-17.
@@ -30,7 +33,6 @@ public class ThirdActivity extends AppCompatActivity {
         //Activar flecha ir hacia atrás
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
         // Recogemos el nombre del activity anterior
          Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
@@ -39,14 +41,48 @@ public class ThirdActivity extends AppCompatActivity {
             typeOfMessage = bundle.getInt("option");
         }
 
+        // Instanciamos los elementos de la UI con sus referencias
+        buttonConfirm = (ImageButton) findViewById(R.id.buttonConfirm);
+        buttonShare = (Button) findViewById(R.id.buttonToShare);
+
 
         // Instanciamos los elementos de la UI con sus referencias
         buttonConfirm = (ImageButton) findViewById(R.id.buttonConfirm);
         buttonShare = (Button) findViewById(R.id.buttonToShare);
 
 
+        // Evento click del botón confirmar
+        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ThirdActivity.this, createMessage(name, age, typeOfMessage), Toast.LENGTH_LONG).show();
+                buttonShare.setVisibility(View.VISIBLE);
+                buttonConfirm.setVisibility(View.INVISIBLE);
+            }
+        });
 
-
-
+        // Evento click del botón share
+        buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, createMessage(name, age, typeOfMessage));
+                startActivity(intent);
+            }
+        });
     }
+
+
+        private String createMessage(String name, int age, int typeOfMessage) {
+            if (typeOfMessage == SecondActivity.GREETER_OPTION) {
+                return "Hola " + name + ", ¿Cómo llevas esos " + age + " años? #MyForm";
+            } else {
+                return "Espero verte pronto " + name + ", antes que cumplas " + (age + 1) + ".. #MyForm";
+            }
+        }
+
+
+
+
 }
