4ce68a6e9c43b2e880845c3b3e6dd7626db1bea2
german <germanjongewaard@gmail.com>
2017-11-09 16:35:27 +0100

    first commit.

--- app/src/main/java/dev/com/jongewaard/my_form/MainActivity.java
+++ app/src/main/java/dev/com/jongewaard/my_form/MainActivity.java
@@ -5,6 +5,7 @@ package dev.com.jongewaard.my_form;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,12 +23,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Forzar el logo, en todas las versiones android
-        getSupportActionBar().setLogo(R.mipmap.form);
+        getSupportActionBar().setLogo(R.mipmap.ic_form);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
-
-        //Forzar y cargar  icono en el Action Bar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.mipmap.form);
+
+
+
+
+
 
         // Instanciamos los elementos de la UI con sus referencias
         eTxt_Intro_Name = findViewById(R.id.editText_Intro_Name);
